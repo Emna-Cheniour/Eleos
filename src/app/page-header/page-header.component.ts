bb133739c18fd661e5493b3c72cbf6701268b8b3
Haythem <haythem@gmail.com>
2022-12-28 13:57:07 +0100

    routing and design

--- src/app/page-header/page-header.component.ts
+++ src/app/page-header/page-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-header',
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class PageHeaderComponent implements OnInit {
 
   constructor() { }
-
+  @Input() pageName:string | undefined;
   ngOnInit(): void {
   }
 
