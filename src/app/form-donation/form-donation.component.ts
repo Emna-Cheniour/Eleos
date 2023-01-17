2f5a59d4fc845c4adf74862ffc683b70bc9b47f1
Emna Cheniour <emnacheniour@insat.u-carthage.tn>
2023-01-17 18:09:13 +0100

    articles

--- src/app/form-donation/form-donation.component.ts
+++ src/app/form-donation/form-donation.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
@@ -7,38 +8,43 @@ import { FormBuilder, FormControl, FormGroup, NgForm, Validators } from '@angula
   styleUrls: ['./form-donation.component.css']
 })
 export class FormDonationComponent implements OnInit {
-  shortLink: string = "";
-  loading: boolean = false; 
-  file!: File; 
+
   donationInfo: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private http: HttpClient) {
     this.donationInfo = this.fb.group({
-      name: ['', Validators.required],
+      name:  ['', Validators.required],
       description: ['', Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(1000)])],
-      type: ['', Validators.compose([Validators.required])],
-      file: ['', Validators.compose([Validators.required])],
+      categorie:  ['', Validators.compose([Validators.required])],
+      file:  ['', Validators.compose([Validators.required])],
       fileSource: new FormControl('', [Validators.required]),
     });
   }
 
   ngOnInit(): void {
   }
+
+  get f(){
+    return this.donationInfo.controls;
+  }
+
   addDonation(values: any) {
     const productData = new FormData();
     productData.append('name', values.name);
     productData.append('description', values.description);
-    productData.append('type', values.type);
-    productData.append('file', this.donationInfo.get('fileSource')?.value);
-    console.log(values);
+    productData.append('categorie', values.categorie);
+    // productData.append('fileSource', this.donationInfo.get('fileSource')?.value);
+ 
+    this.http
+      .post('http://localhost:4000/eleos/articles', productData)
+      .subscribe({
+        next: (response) => console.log(response),
+        error: (error) => console.log(error),
+      });
 
-    // this.crudService.createProduct(productData).subscribe(() => {
-    //   this.router.navigate(['']);
-    // });
-  
   }
-  onFileChange(event:any) {
-  
+  onFileChange(event: any) {
+
     if (event.target.files.length > 0) {
       const file = event.target.files[0];
       this.donationInfo.patchValue({
